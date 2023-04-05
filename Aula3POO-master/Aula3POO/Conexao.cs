8e96e13a22148d0d6cc38e3d9b6ea84768c7ac17
ArthurVeraci <105826714+ArthurVeraci@users.noreply.github.com>
2023-04-05 19:10:28 -0300

    Add files via upload

---
+++ Aula3POO-master/Aula3POO/Conexao.cs
@@ -0,0 +1,92 @@
+﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Cms;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula3POO
+{
+    internal class Conexao
+    {
+        static private string server = "localhost";
+        static private string database = "bd_aula3";
+        static private string usuario = "root";
+        static private string senha = "cursoads";
+
+        static public string strConn = $"server={server};" +
+            $"database={database}; User Id={usuario};" +
+            $"password={senha};";
+
+        MySqlConnection cn;
+
+        private bool Conectar()
+        {
+            bool result;
+            try
+            {
+                cn = new MySqlConnection(strConn);
+                cn.Open();
+                result = true;
+            }
+            catch
+            {
+                result = false;
+            }
+            return result;
+        }
+
+        private void Desconectar()
+        {
+            if (cn.State == System.Data.ConnectionState.Open)
+            {
+                cn.Close();
+            }
+        }
+        public bool Executa(string sql)
+        {
+            bool resultado = false;
+            if (Conectar())
+            {
+                try
+                {
+                    MySqlCommand cmd = new MySqlCommand(sql, cn);
+                    cmd.ExecuteNonQuery();
+                    resultado = true;
+                }
+                catch
+                {
+                    resultado = false;
+                }
+                finally
+                {
+                    cn.Close();
+                }
+            }
+            return resultado;
+        }
+        public DataTable Retorna(string sql)
+        {
+            Conectar();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(sql, cn);
+                MySqlDataAdapter da = new MySqlDataAdapter();
+                da.SelectCommand = cmd;
+                DataTable data = new DataTable();
+                da.Fill(data);
+                return data;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                Desconectar();
+            }
+        }
+    }
+}
---
+++ Aula3POO-master/Aula3POO/TelaPrincipal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula3POO
+{
+    public partial class TelaPrincipal : Form
+    {
+        public TelaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        Form tela; //declaração do form
+        private void btnCliente_Click(object sender, EventArgs e)
+        {
+            tela = new TelaCliente //instanciação da tela
+            {
+                TopLevel = false,
+                FormBorderStyle = FormBorderStyle.None,
+                Dock = DockStyle.Fill,
+            };
+            pnlTela.Controls.Add(tela);
+            tela.Show();
+        }
+
+        private void btnProduto_Click(object sender, EventArgs e)
+        {
+            tela?.Close();
+            tela = new TelaProduto
+            {
+                TopLevel = false,
+                FormBorderStyle = FormBorderStyle.None,
+                Dock = DockStyle.Fill,
+            };
+            pnlTela.Controls.Add(tela);
+            tela.Show();
+        }
+    }
+}
